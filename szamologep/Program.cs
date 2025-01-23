c110ca7383f3dda52b0cebb8dac92a968aa0bc7a
Levente Rasztovits <rasztovits.levente@petrik.hu>
2025-01-24 00:16:12 +0100

    Muvelet_Kivalasztasa Kész

--- szamologep/Program.cs
+++ szamologep/Program.cs
@@ -23,13 +23,47 @@ namespace szamologep
             return szam;
         }
 
+        static string Muvelet_Kivalasztasa()
+        {
+            Console.WriteLine("Lehetőségek: '+' '-' '/' '*' '%' ");
+            Console.WriteLine("Elvégezndő művelet jele: ");
+            string muvelet = Console.ReadLine();
+            return muvelet;
+        }
+
         static void Main(string[] args)
         {
             double szam1 = Elso_Szam();
             double szam2 = Masodik_Szam();
+            string muvelet = Muvelet_Kivalasztasa();
 
             Console.WriteLine($"Az első szám: {szam1}");
             Console.WriteLine($"A második szám: {szam2}");
+
+            if(muvelet == "+")
+            {
+                Console.WriteLine($"Az összeg: {Osszeadas}");
+            }
+            else if (Muvelet_Kivalasztasa() == "-")
+            {
+                Console.WriteLine($"A különbség: {Kulonbseg()}");
+            }
+            else if (Muvelet_Kivalasztasa() == "/")
+            {
+                Console.WriteLine($"Az osztás eredménye: {Osztas}");
+            }
+            else if (Muvelet_Kivalasztasa() == "*")
+            {
+                Console.WriteLine($"A szorzás eredménye: {Szorzas()}");
+            }
+            else if (Muvelet_Kivalasztasa() == "%")
+            {
+                Console.WriteLine($"A százaékos osztás eredménye: {Szazalek()}");
+            }
+            else
+            {
+                Console.WriteLine("Hibás művelet!");
+            }
 
         }
 
