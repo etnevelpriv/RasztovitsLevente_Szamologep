b6157e3e8e82092dfabaec0d83777b5613ba5659
Levente Rasztovits <rasztovits.levente@petrik.hu>
2025-01-24 00:00:09 +0100

    Add project files.

---
+++ szamologep/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szamologep
+{
+    internal class Program
+    {
+
+        static double Elso_Szam()
+        {
+            Console.Write("Kérem adja meg az első számot: ");
+            double szam = Convert.ToDouble(Console.ReadLine());
+            return szam;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Az első szám: {Elso_Szam()}");
+        }
+
+    }
+}
